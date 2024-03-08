f9fe87dc87555af1cb3a14505d3a72152099e4ad
GuilhermeLinhares04 <guilherme.linhares@sou.inteli.edu.br>
2024-03-08 15:47:08 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+Feito por: Guilherme Ferreira Linhares - Innstituto de tecnologia e liderança - Turma 8 Engenharia da Computação 2024
+
+Como executar o projeto:
---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, url_for
+from tinydb import TinyDB, Query
+
+app = Flask(__name__)
+db = TinyDB('caminhos.json')
+robot_table = db.table('robot')
+
+# Rota para listar as posições
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Rota para postar as posições e guardá-las na database
+@app.route('/novo', methods=['POST'])
+def post():
+    x = request.form['x']
+    y = request.form['y']
+    z = request.form['z']
+    r = request.form['r']
+    robot_table.insert({'x': x, 'y': y, 'z': z, 'r': r})
+    return render_template('caminho.html', message=('Posição salva com sucesso!'))
+
+# Rota para receber o id do caminho e devolver as posições
+@app.route('/pegar_caminho/<int:id>')
+def caminho(id):
+    robot = Query()
+    result = robot_table.get(doc_id=id)
+    return render_template('caminho.html', result=result)
+
+# Rota para listar todos os caminhos
+@app.route('/listar_caminhos')
+def caminhos():
+    caminhos = robot_table.all()
+    return render_template('caminhos.html', caminhos=caminhos)
+
+# Rota para atualizar as posições
+@app.route('/atualizar_caminho/<int:id>', methods=['POST'])
+def atualizar_caminho(id):
+    robot = Query()
+    x = request.form['x']
+    y = request.form['y']
+    z = request.form['z']
+    r = request.form['r']
+    robot_table.update({'x': x, 'y': y, 'z': z, 'r': r}, doc_ids=[id])
+    return redirect(url_for('caminhos'))
+
+# Rota para deletar um caminho
+@app.route('/deletar_caminho/<int:id>')
+def deletar_caminho(id):
+    robot_table.remove(doc_ids=[id])
+    return redirect(url_for('caminhos'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
